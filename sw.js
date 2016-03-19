ed1799c101c1778a76498040040f732adc209a35
Ruben Martinez Jr <ruben.martinez93@gmail.com>
2016-03-18 22:07:49 -0700

    Adding Service Worker

---
+++ sw.js
@@ -0,0 +1,28 @@
+/* Cache Polyfill https://github.com/coonsta/cache-polyfill/blob/master/index.js */
+Cache.prototype.addAll||(Cache.prototype.addAll=function(t){function e(t){this.name='NetworkError',this.code=19,this.message=t}var r=this;return e.prototype=Object.create(Error.prototype),Promise.resolve().then(function(){if(arguments.length<1)throw new TypeError;return t=t.map(function(t){return t instanceof Request?t:String(t)}),Promise.all(t.map(function(t){"string"==typeof t&&(t=new Request(t));var r=new URL(t.url).protocol;if("http:"!==r&&"https:"!==r)throw new e("Invalid scheme");return fetch(t.clone())}))}).then(function(e){return Promise.all(e.map(function(e,n){return r.put(t[n],e)}))}).then(function(){return void 0})});
+/* ------------- */
+
+importScripts('/js/sw-toolbox/sw-toolbox.js');
+
+//do what's fastest and update the cache
+toolbox.router.default = toolbox.fastest;
+toolbox.router.any('/(.*)', toolbox.cacheFirst, { origin: 'https://ajax.googleapis.com' });
+toolbox.router.any('/(.*)', toolbox.cacheFirst, { origin: 'https://maxcdn.bootstrapcdn.com' });
+
+const urlsToCache = [
+  '/index.html',
+  '/sw_install.js',
+  '/sw.js',
+  '/js/sw-toolbox/sw-toolbox.js',
+  '/js/flaticon.js',
+  '/js/colpick.js',
+  '/js/rangeslider.min.js',
+  '/css/colpick.css',
+  '/css/default.css',
+  '/css/flat-ui.min.css',
+  '/css/rangeslider.css',
+  '/img/bolt.png',
+  '/img/icon.png'
+];
+
+toolbox.precache(urlsToCache);
